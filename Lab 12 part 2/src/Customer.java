60002153197bb0b8b425bb143766dacab137aa2e
watso <gwatso138@gmail.com>
2024-04-07 14:24:59 +0100

    Initial Lab 12 part 2

---
+++ Lab 12 part 2/src/Customer.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.google.gson.*;
+public class Customer {
+    String CustomerID, CompanyName, ContactName, ContactTitle;
+    String Address, PostCode, Country,Phone, Fax;
+    public static void main(String[] args)throws IOException{
+        Gson gson = new Gson();//Create new instance of Gson Class
+   	    String fileName = "/home/watso/eclipse-workspace/Lab 12 part 2/src/JSONFile.json";//Give the path to the file
+        String content = new String(Files.readAllBytes(Paths.get(fileName)));//Read file at path into String
+   	    Customer[] customers = gson.fromJson(content, Customer[].class);//fromJson() converts Json content to Java objects
+   	    System.out.println("\tCustID\tContact Name\tPhoneNo"); //Now we have an array of customers to Loop
+   	    for(int i=0; i < customers.length;i++) {
+   	    System.out.print("\t" + customers[i].CustomerID);
+   	    System.out.print("\t" + customers[i].ContactName);
+   	    System.out.println("\t" + customers[i].Phone);
+   	    }
+   	    int[] nos = {1,3,5,7,9};
+		String res = gson.toJson(nos);
+		System.out.println(res);
+
+		String[] names= {"Bob","Fred","Wilma"};
+		res = gson.toJson(names);
+		System.out.println(res);
+		//Nb: Changed Class to StudenInst as I couldn't remember whether we had a Student Class defined already - Lol
+		StudentInst[] students = {
+				new StudentInst("Bob",21), 
+				new StudentInst("Fred",32), 
+				new StudentInst("Wilma",26)
+		};
+		res = gson.toJson(students);
+		System.out.println(res);
+   	    }
+}
+
+
---
+++ Lab 12 part 2/src/StudentInst.java
@@ -0,0 +1,26 @@
+
+public class StudentInst {
+private String name;
+private int age;
+public StudentInst() {
+	super();
+	}
+public String getName() {
+	return name;
+}
+public StudentInst(String name, int age) {
+	super();
+	this.name = name;
+	this.age = age;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+
+}
