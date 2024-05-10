dcde08123b4033fa87a8e48fdad70ceac4545115
watso <gwatso138@gmail.com>
2024-05-10 11:39:00 +0100

    commented part 1

--- QA_Lab06/src/com/qa/main/jslabs/lab06/Lab6.java
+++ QA_Lab06/src/com/qa/main/jslabs/lab06/Lab6.java
@@ -4,46 +4,45 @@ import java.util.Scanner;
 
 public class Lab6 {
 	public void part1() {
-		grades();
-	}
+		grades();   // Calls method grades()
 
 	public void part2() {
 		account();
 	}
 	
 	public void grades() {
-		int numStudents = 5;
-		String[] names = new String[numStudents];
-		int[] marks = new int[numStudents];
+		int numStudents = 5; // No of Students is 5
+		String[] names = new String[numStudents];// create array of size numStudents to hold students
+		int[] marks = new int[numStudents];// create array of size numStudents to hold marks
 		
-		for (int i = 0; i < marks.length; i++) {
-			String nameText = String.format("Enter name for student %d: ", i);
-			names[i] = getString(nameText);
+		for (int i = 0; i < marks.length; i++) { //iterate over ,marks array
+			String nameText = String.format("Enter name for student %d: ", i); //create a user prompt for the student name assign to String nameText
+			names[i] = getString(nameText); //Call getString() which uses a Scanner to capture user input & assign to names[] Array
 			
-			String markText = String.format("Enter a grade mark between 1..100 for student %d: ", i);
+			String markText = String.format("Enter a grade mark between 1..100 for student %d: ", i);// create a prompt for mark user input as String markText
 			while (true)
 			{
-				String inputMark = getString(markText);
-				Integer mark = tryParseInt(inputMark);
+				String inputMark = getString(markText); // use getString(with our prompt) to get user input for a mark assign to inputMark
+				Integer mark = tryParseInt(inputMark);  // use Integer.tryParseInt(with our prompt) to get user input for a mark assign to mark (now be int)
 				// Check input is valid.
 				if (mark == null) {
-					System.out.println("Error: Value must be an integer. Try again.");
+					System.out.println("Error: Value must be an integer. Try again.");//Error if mark null
 				} else if (!(mark > 0 && mark <= 100)) {
-					System.out.println("Error: marks must be between 1..100. Try again.");
+					System.out.println("Error: marks must be between 1..100. Try again.");// Error if mark not between 0..100
 				} else {
-					marks[i] = mark;
+					marks[i] = mark; // Add the mark to the array if it is a valid int between 0..100
 					break;
 				}
 			}
 		}
 		
-		for (int i = 0; i < marks.length; i++) {
-			String resultText = "Grade for student %d is: %s\n";
+		for (int i = 0; i < marks.length; i++) { //iterate through marks
+			String resultText = "Grade for student %d is: %s\n"; //Building partial output String for the common text
 			// Get result
-			if (marks[i] >= 71) {
-				System.out.printf(resultText, i, "Distinction");
+			if (marks[i] >= 71) { //Check the mark
+				System.out.printf(resultText, i, "Distinction"); // Populated the partial output and printed to screen Note use of printf
 			}
-			else if (marks[i] >= 61 && marks[i] <= 70) {
+			else if (marks[i] >= 61 && marks[i] <= 70) { // rinse and repeat for other mark ranges
 				System.out.printf(resultText, i, "Merit");
 			}
 			else if (marks[i] >= 50 && marks[i] <= 60) {
