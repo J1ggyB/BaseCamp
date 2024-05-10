d39a1c497a83e239c5928015f3d1f0cc9579ba7d
watso <gwatso138@gmail.com>
2024-05-10 14:15:28 +0100

    mfix

--- QA_Lab07/src/com/qa/main/jslabs/lab07/Program2.java
+++ QA_Lab07/src/com/qa/main/jslabs/lab07/Program2.java
@@ -3,7 +3,7 @@ package com.qa.main.jslabs.lab07;
 public class Program2 {
 
 	public static void main(String[] args) {
-		// Lab 7 part 1
+		// Lab 7 part 2
 		Lab7 lab7 = new Lab7();//Print out each item in the original list
 		int[] numbers = { 1, 3, -5, 7, 0, 4, 6, 8 };
 		
