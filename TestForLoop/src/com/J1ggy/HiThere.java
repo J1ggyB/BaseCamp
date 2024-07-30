cc9daefce7e3171bc4ee0eef13e78cde9343028d
watso <gwatso138@gmail.com>
2024-07-30 16:42:21 +0100

    Test For init commit

---
+++ TestForLoop/src/com/J1ggy/HiThere.java
@@ -0,0 +1,16 @@
+package com.J1ggy;
+
+public class HiThere {
+
+	public static void main(String[] args) {
+
+int[] nums = new int[]{1,2,3,4,5,6,7,8,9};
+int sum = 0;
+for(int i=0 ; i < nums.length; i++) {
+	sum += nums[i];
+}
+System.out.println(sum);
+	}
+
+}
+ 
