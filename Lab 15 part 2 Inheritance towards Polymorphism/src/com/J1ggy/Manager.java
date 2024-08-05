f369be7e3da5f141360adb119864a17bd44b0b47
watso <gwatso138@gmail.com>
2024-08-05 14:05:24 +0100

    m

---
+++ Lab 15 part 2 Inheritance towards Polymorphism/src/com/J1ggy/Manager.java
@@ -0,0 +1,25 @@
+package com.J1ggy;
+
+import java.util.ArrayList;
+
+public class Manager extends Employee{
+	//--Fields-----------------------------------------
+	//public ArrayList<Employee>employees = new ArrayList<>(); - Moved to Employee (Parent)
+	String info;
+	//--Constructor---------------------------------------
+	public Manager(String name, String jobTitle) {
+		super(name, jobTitle);
+
+	}
+	//--Methods---------------------------------------
+public void addEmployee(Employee emp) {
+	employees.add(emp);
+	
+}
+public String getInfo() { // Call the parent getInfo
+	String info =super.getInfo();
+	return info;
+}
+
+
+}//END CLASS
